54f05f9e38d00d78ea490c48c44741daae8ff57f
Fahrudin Nasikh Az zuhdu <19523005@students.uii.ac.id>
2022-08-24 09:06:43 +0700

    create detail page

--- app/src/main/java/com/example/homebantoo/ui/detail.kt
+++ app/src/main/java/com/example/homebantoo/ui/detail.kt
@@ -1,11 +1,12 @@
-package com.example.homebantoo
+package com.example.homebantoo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.homebantoo.R
 
-class MainActivity : AppCompatActivity() {
+class detail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detail)
     }
 }
